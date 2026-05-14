f756defdc6a706ec3faecb56adc28b3020c7e16f
Anton <anton.zhurauliou@gmail.com>
2026-05-14 16:26:03 +0300

    chore(lint): add ruff, mypy and pre-commit configuration

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from config import settings
@@ -27,7 +28,7 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
